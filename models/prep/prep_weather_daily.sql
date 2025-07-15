f9efcbfd91ef63ca65bd39a9d03d8cbfe4393ef6
sakr26 <s.krauss-roskamm@gmx.net>
2025-07-15 12:21:31 +0200

    updated dbt project, added prep_weather model

---
+++ models/prep/prep_weather_daily.sql
@@ -0,0 +1,27 @@
+WITH daily_data AS (
+    SELECT * 
+    FROM {{ref('staging_weather_daily')}}
+),
+add_features AS (
+    SELECT *
+		, DATE_PART('day', date) AS date_day 		-- number of the day of month
+		, DATE_PART('month', date) AS date_month 	-- number of the month of year
+		, DATE_PART('year', date) AS date_year 		-- number of year
+		, DATE_PART('week', date) AS cw 			-- number of the week of year
+		, TO_CHAR(date, 'Month') AS month_name 	-- name of the month
+		, TO_CHAR(date, 'Day') AS weekday 		-- name of the weekday
+    FROM daily_data 
+),
+add_more_features AS (
+    SELECT *
+		, (CASE 
+			WHEN month_name in ('December', 'January', 'February') THEN 'winter'
+			WHEN ('March', 'April', 'May') THEN 'spring'
+            WHEN ('June', 'July', 'August') THEN 'summer'
+            WHEN ('September', 'October', 'November') THEN 'autumn'
+		END) AS season
+    FROM add_features
+)
+SELECT *
+FROM add_more_features
+ORDER BY date
